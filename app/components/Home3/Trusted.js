40f2a4565fc849a5df77f0d942e3b14f735870cc
Jewel-190237 <190237@ku.ac.bd>
2024-10-14 14:28:35 +0600

    trusted updated

--- app/components/Home3/Trusted.js
+++ app/components/Home3/Trusted.js
@@ -1,6 +1,8 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import Marquee from 'react-marquee-slider';
 
 const Trusted = () => {
     const TrustedData = [
@@ -10,26 +12,35 @@ const Trusted = () => {
         { image: '/trusted/dropBox.svg' },
         { image: '/trusted/webflow.svg' },
         { image: '/trusted/arbnb.svg' },
-    ]
+    ];
+
+    const autofillData = [...TrustedData, ...TrustedData]; // Duplicate the array
 
     return (
         <section className='bg-[#101928]'>
             <div className='flex items-center justify-between'>
                 <div className='hidden xl:flex items-center justify-end -mt-24'>
-                    <Image src='/trusted/leftImage.svg' width={160} height={415} alt='Right site style' />
+                    <Image src='/trusted/leftImage.svg' width={160} height={415} alt='Left side style' />
                 </div>
-                <div className='max-w-[1320px] mx-auto px-8 lg:px-0 py-20'>
+                <div className='mx-auto px-8 lg:px-0 py-20'>
                     <div className="flex justify-center">
-                        <h4 className="trusted-heading">Trusted by 100k+ Investors Company </h4>
+                        <h4 className="trusted-heading">Trusted by 100k+ Investors Company</h4>
                     </div>
-                    <div className='trusted-brand'>
-                        {TrustedData.map((data, index) => (
-                            <Link key={index} href=''> <Image src={data.image} width={135} height={48} alt='Brand Image' /></Link>
-                        ))}
+                    <div className='trusted-brand relative overflow-hidden'>
+                        <div className="absolute inset-0 bg-gradient-to-r from-[#101928] via-transparent to-[#101928] z-10 pointer-events-none" style={{ height: '100%', width: '100%' }} />
+                        <Marquee pauseOnHover={true} className="relative z-20">
+                            {autofillData.map((data, index) => (
+                                <div key={index} style={{ display: 'flex', alignItems: 'center', marginRight: '60px' }}>
+                                    <Link href=''>
+                                        <Image src={data.image} width={135} height={48} alt='Brand Image' />
+                                    </Link>
+                                </div>
+                            ))}
+                        </Marquee>
                     </div>
                 </div>
                 <div className='hidden xl:flex items-center justify-end'>
-                    <Image src='/trusted/rightImage.svg' width={160} height={415} alt='Right site style' />
+                    <Image src='/trusted/rightImage.svg' width={160} height={415} alt='Right side style' />
                 </div>
             </div>
         </section>
